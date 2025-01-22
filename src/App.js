e56fd35cd7a4497798e7fff693e1d1bab1e6e65f
Revanth Upadhyayula <169019945+revanth-upadhyayula@users.noreply.github.com>
2025-01-22 13:29:00 +0530

    half-tutorial-done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
-function App() {
+
+const Person=(props)=>{
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h1>LastName:{props.lastName}</h1>
+      <h1>Age:{props.age}</h1>
+    </>
+  )
+}
+const App = ()=> {
+  const [counter,setCounter] =useState(0);
+  const name="revanth";
+  const isnothing=false;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Hello, {name}!</h1> */}
+      {/* <h1>Hello, {isnothing ? name : 'someone'}!</h1> */}
+      {/* {name ? (
+        <>
+          <h1>{name}</h1>
+        </>
+      ) :(
+        <h2>no name</h2>
+      )
+
+      } */}
+      {/* <Person/> */}
+
+      {/* 
+          ->There some thing called props which is used to change data dynamically.
+          ->If you use Person component for 3 times the app will show same data which was assigned to variables in person component.
+          ->So, to overcome that we use props.
+          ->Props is used to pass data from parent component to child component.
+          ->Below is a small example of how to use props.
+      */}
+      {/* <Person name="John" lastName="Doe" age={30} />
+      <Person name="Mary" lastName="Doe" age={26} />  */}
+
+      {/* 
+          ->state in react is a plain javascript
+            object used by react to represent a 
+            piece of information about the component's 
+            current situation it is completely managed by the component 
+      */}
+
+
+
+      <button onClick={()=>setCounter((prevCount)=>prevCount-1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={()=>setCounter((prevCount)=>prevCount+1)}>+</button>
     </div>
   );
 }
