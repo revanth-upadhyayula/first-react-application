2df2089650bcbf184af7c9764c15924ad097a055
Revanth Upadhyayula <169019945+revanth-upadhyayula@users.noreply.github.com>
2025-01-23 10:26:48 +0530

    Changed src file and copied the preivous src file into another folder for reference

--- src/App.js
+++ src/App.js
@@ -1,58 +1,9 @@
-import {useState} from 'react';
-import './App.css';
+import React from 'react';
 
 
-const Person=(props)=>{
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h1>LastName:{props.lastName}</h1>
-      <h1>Age:{props.age}</h1>
-    </>
-  )
-}
-const App = ()=> {
-  const [counter,setCounter] =useState(0);
-  const name="revanth";
-  const isnothing=false;
-  return (
-    <div className="App">
-      {/* <h1>Hello, {name}!</h1> */}
-      {/* <h1>Hello, {isnothing ? name : 'someone'}!</h1> */}
-      {/* {name ? (
-        <>
-          <h1>{name}</h1>
-        </>
-      ) :(
-        <h2>no name</h2>
-      )
-
-      } */}
-      {/* <Person/> */}
-
-      {/* 
-          ->There some thing called props which is used to change data dynamically.
-          ->If you use Person component for 3 times the app will show same data which was assigned to variables in person component.
-          ->So, to overcome that we use props.
-          ->Props is used to pass data from parent component to child component.
-          ->Below is a small example of how to use props.
-      */}
-      {/* <Person name="John" lastName="Doe" age={30} />
-      <Person name="Mary" lastName="Doe" age={26} />  */}
-
-      {/* 
-          ->state in react is a plain javascript
-            object used by react to represent a 
-            piece of information about the component's 
-            current situation it is completely managed by the component 
-      */}
-
-
-
-      <button onClick={()=>setCounter((prevCount)=>prevCount-1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={()=>setCounter((prevCount)=>prevCount+1)}>+</button>
-    </div>
+const App=()=>{
+  return(
+    <h1>App</h1>
   );
 }
 
